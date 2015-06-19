a7cf1a882963531ebe155c07627512fcef541725
Maciej Dziardziel <fiedzia@gmail.com>
2015-06-20 00:53:27 +0100

    solve issue with dbshell asking for password

--- django_pgcli/__init__.py
+++ django_pgcli/__init__.py
@@ -12,5 +12,21 @@ from django.db.backends.postgresql_psycopg2.client import DatabaseClient
 class pgcliDatabaseClient(DatabaseClient):
     executable_name = 'pgcli'
 
+    def runshell(self):
+        from pgcli.main import cli
+
+        settings_dict = self.connection.settings_dict
+        args = []
+
+        if settings_dict['USER']:
+            args += ['--user', settings_dict['USER']]
+        if settings_dict['HOST']:
+            args += ['--host', settings_dict['HOST']]
+        if settings_dict['PORT']:
+            args += ['--port', settings_dict['PORT']]
+        args += [settings_dict['NAME']]
+        cli(args=args)
+
+
 base.__old_database_client = base.DatabaseClient
 base.DatabaseClient = pgcliDatabaseClient
