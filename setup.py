fdf30874f3f649427fb273eb87b5c701ed7343f8
Ash Christopher <ash.christopher@gmail.com>
2018-03-09 15:55:27 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -50,7 +50,6 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Lanugage :: Python :: 3.6',
     ],
     test_suite='tests',
     tests_require=test_requirements
