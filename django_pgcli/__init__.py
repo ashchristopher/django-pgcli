4255d43ef755723b3b1ccd11269c19866fbb5d87
Junwei Yu <junweiyu@berkeley.edu>
2018-03-08 16:03:35 -0800

    enable compatibility with Django >= 1.9 and drop support for Django 1.8 and below

--- django_pgcli/__init__.py
+++ django_pgcli/__init__.py
@@ -2,15 +2,18 @@
 
 __author__ = 'Ash Christopher'
 __email__ = 'ash.christopher@gmail.com'
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 
-from django.db.backends.postgresql_psycopg2 import base
-from django.db.backends.postgresql_psycopg2.client import DatabaseClient
+from django.db.backends.postgresql import base
+from django.db.backends.postgresql.client import DatabaseClient
 
 
 class pgcliDatabaseClient(DatabaseClient):
     executable_name = 'pgcli'
+    
+    def runshell(self):
+        pgcliDatabaseClient.runshell_db(self.connection.get_connection_params())
 
-base.__old_database_client = base.DatabaseClient
-base.DatabaseClient = pgcliDatabaseClient
+base.DatabaseWrapper.__old_database_client_class = base.DatabaseClient
+base.DatabaseWrapper.client_class = pgcliDatabaseClient
